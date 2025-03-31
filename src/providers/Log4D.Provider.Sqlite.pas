9826bbc447ddbdb458c969debffaa4a18a672267
Gabriel Fabrega <gabriel.fabrega@outlook.com>
2025-03-31 18:04:29 -0300

    fix: Ajustando compatibilidade

--- src/providers/Log4D.Provider.Sqlite.pas
+++ src/providers/Log4D.Provider.Sqlite.pas
@@ -3,26 +3,31 @@ unit Log4D.Provider.Sqlite;
 interface
 
 uses
-  System.SysUtils,
-  System.IOUtils,
-  Log4D.Provider,
-  Log4D.Provider.Param.Files,
-  Log4D,
-  FireDAC.Comp.Client,
-  FireDAC.Phys.SQLite,
-  FireDAC.Stan.Async,
-  FireDAC.Stan.Def,
-  FireDAC.UI.Intf,
-  FireDAC.VCLUI.Wait,
-  FireDAC.Stan.Intf,
-  FireDAC.Comp.UI;
+  Log4D.Provider.Param.Files
+  ,Log4D.Provider
+  ,System.IOUtils
+  ,System.SysUtils
 
+  {$IF RTLVersion >= 30.0, System.SysUtils}
+    ,FireDAC.Comp.Client
+    ,FireDAC.Phys.SQLite
+    ,FireDAC.Stan.Async
+    ,FireDAC.Stan.Def
+    ,FireDAC.UI.Intf
+    ,FireDAC.VCLUI.Wait
+    ,FireDAC.Stan.Intf
+    ,FireDAC.Comp.UI;
+  {$ELSE}
+    ;
+  {$IFEND}
 
 type
   TLog4DProviderSqlite = class(TInterfacedObject,ILog4DProvider)
   private
-    FConnection: TFDConnection;
-    FCursor: TFDGUIxWaitCursor;
+    {$IF RTLVersion >= 30.0}
+      FConnection: TFDConnection;
+      FCursor: TFDGUIxWaitCursor;
+    {$IFEND}
     FParams: TLog4DProviderParamFiles;
     procedure CreateDatabase;
     procedure CreateLogTable;
@@ -39,49 +44,57 @@ implementation
 constructor TLog4DProviderSqlite.Create(AParams: TLog4DProviderParamFiles);
 begin
   FParams := AParams;
-  FCursor := TFDGUIxWaitCursor.Create(nil);
+  {$IF RTLVersion >= 30.0}
+    FCursor := TFDGUIxWaitCursor.Create(nil);
+  {$ELSE}
+    raise Exception.Create('Provedor SQLite disponível apenas para versões superiores à Seattle')
+  {$IFEND}
 end;
 
 procedure TLog4DProviderSqlite.Send(AMessage: string);
 begin
-  FConnection := TFDConnection.Create(nil);
-  try
+  {$IF RTLVersion >= 30.0}
+    FConnection := TFDConnection.Create(nil);
     try
-      CreateDatabase;
-      FConnection.ExecSQL(
-        'INSERT INTO application_log (message) ' +
-        'VALUES (' + QuotedStr(AMessage) + ')');
-    except on e:Exception do
-      TLog4D.Error(e.Message,false);
+      try
+        CreateDatabase;
+        FConnection.ExecSQL(
+          'INSERT INTO application_log (message) ' +
+          'VALUES (' + QuotedStr(AMessage) + ')');
+      except on e:Exception do
+        TLog4D.Error(e.Message,false);
+      end;
+    finally
+      FConnection.Free;
     end;
-  finally
-    FConnection.Free;
-  end;
+  {$IFEND}
 end;
 
 procedure TLog4DProviderSqlite.CreateDatabase;
 var
   LDatabasePath: String;
 begin
-  LDatabasePath := TPath.Combine(FParams.Path,FParams.FileName);
-  FConnection.DriverName := 'SQLite';
-  FConnection.Params.Values['Database'] := LDatabasePath;
+  {$IF RTLVersion >= 30.0}
+    LDatabasePath := TPath.Combine(FParams.Path,FParams.FileName);
+    FConnection.DriverName := 'SQLite';
+    FConnection.Params.Values['Database'] := LDatabasePath;
 
-  if not FileExists(LDatabasePath) then
-  begin
-    FConnection.Params.Values['OpenMode'] := 'CreateUTF8';
+    if not FileExists(LDatabasePath) then
+    begin
+      FConnection.Params.Values['OpenMode'] := 'CreateUTF8';
+      FConnection.Connected := True;
+      FConnection.Connected := False;
+      FConnection.Params.Values['OpenMode'] := 'ReadWrite';
+    end;
+
+    FConnection.Params.Values['LockingMode'] := 'Normal';
+    FConnection.Params.Values['Synchronous'] := 'Normal';
+    FConnection.Params.Values['JournalMode'] := 'WAL';
+    FConnection.Params.Values['StringFormat'] := 'Unicode';
+
     FConnection.Connected := True;
-    FConnection.Connected := False;
-    FConnection.Params.Values['OpenMode'] := 'ReadWrite';
-  end;
-
-  FConnection.Params.Values['LockingMode'] := 'Normal';
-  FConnection.Params.Values['Synchronous'] := 'Normal';
-  FConnection.Params.Values['JournalMode'] := 'WAL';
-  FConnection.Params.Values['StringFormat'] := 'Unicode';
-
-  FConnection.Connected := True;
-  CreateLogTable;
+    CreateLogTable;
+  {$IFEND}
 end;
 
 procedure TLog4DProviderSqlite.CreateLogTable;
@@ -95,15 +108,18 @@ const
     '  user TEXT' +
     ')';
 begin
-  FConnection.ExecSQL(SQL_CREATE_TABLE);
-
-  // Cria �ndice para melhor performance em consultas por data
-  FConnection.ExecSQL('CREATE INDEX IF NOT EXISTS idx_log_time ON application_log(log_time)');
+  {$IF RTLVersion >= 30.0}
+    FConnection.ExecSQL(SQL_CREATE_TABLE);
+    // Cria índice para melhor performance em consultas por data
+    FConnection.ExecSQL('CREATE INDEX IF NOT EXISTS idx_log_time ON application_log(log_time)');
+  {$IFEND}
 end;
 
 destructor TLog4DProviderSqlite.Destroy;
 begin
-  FCursor.Free;
+  {$IF RTLVersion >= 30.0}
+    FCursor.Free;
+  {$IFEND}
   inherited;
 end;
 
